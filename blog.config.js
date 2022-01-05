a9a47497cbbde1b853de670d3ccaaa1975f6dc5e
dingdingxia <dingdingxia0207@gmail.com>
2022-01-05 09:37:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: 'SecludedDog',
+  author: 'SecludedDog',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
