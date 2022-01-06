ef33235e16c5d4604046400ce1c9888de0ef9ce1
dingdingxia <dingdingxia0207@gmail.com>
2022-01-06 16:39:50 +0800

    Update layout.js
    add test table of content

--- layouts/layout.js
+++ layouts/layout.js
@@ -21,6 +21,8 @@ const Layout = ({
 }) => {
   const locale = useLocale()
   const router = useRouter()
+  const showTableOfContents = true // add test table of content
+  const minTableOfContentsItems = 3 // add test table of content
   return (
     <Container
       layout="blog"
